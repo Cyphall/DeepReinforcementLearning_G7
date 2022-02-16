372268884d4d3b4a63abcf48a8acb382bc8b5c87
Lior <diler.lior@gmail.com>
2022-02-16 18:42:11 +0100

    Added level generation for gridworld

---
+++ Assets/GridWorld/Scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.WSA;
+
+// ReSharper disable StringLiteralTypo
+
+namespace GridWorld.Scripts
+{
+    public class LevelManager : MonoBehaviour
+    {
+        public List<TileToPrefab> TileToPrefabs;
+
+        public LevelPreset level;
+
+        private void Start()
+        {
+            for (int x = 0; x < level.Grid.Width; ++x)
+            {
+                for (int y = 0; y < level.Grid.Height; ++y)
+                {
+                    TileType tileType = level.Grid.GridTiles[x, y];
+                    GameObject prefab = null;
+                    foreach (var tile in TileToPrefabs)
+                    {
+                        if (tile.TileType == tileType)
+                        {
+                            Instantiate(tile.Prefab, new Vector3(x, 0, y), Quaternion.identity);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
