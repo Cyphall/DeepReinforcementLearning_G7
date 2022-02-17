411f96994776c33047dee25ffbdb9b25f9f8f8ef
Cyphall <cyphallbraus@gmail.com>
2022-02-17 17:49:20 +0100

    sokoban level 1

--- Assets/Sokoban/Scripts/LevelPresets/Level1Preset.cs
+++ Assets/Sokoban/Scripts/LevelPresets/Level1Preset.cs
@@ -7,18 +7,22 @@ namespace Sokoban
 		public Level1Preset()
 		{
 			Grid = new TileType[,]{
-				{ TileType.Wall, TileType.Wall,   TileType.Wall,   TileType.Wall,   TileType.Wall,   TileType.Wall}, 
-				{ TileType.Wall, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Button,   TileType.Wall},
-				{ TileType.Wall, TileType.Ground, TileType.Wall,   TileType.Ground, TileType.Ground, TileType.Wall},
-				{ TileType.Wall, TileType.Button, TileType.Ground, TileType.Wall,   TileType.Ground, TileType.Wall},
-				{ TileType.Wall, TileType.Ground, TileType.Button, TileType.Ground, TileType.Ground, TileType.Wall},
-				{ TileType.Wall, TileType.Wall,   TileType.Wall,   TileType.Wall,   TileType.Wall,   TileType.Wall}
+				{ TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall}, 
+				{ TileType.Wall, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Wall},
+				{ TileType.Wall, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Wall},
+				{ TileType.Wall, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Wall, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Wall},
+				{ TileType.Wall, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Wall, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Wall},
+				{ TileType.Wall, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Wall, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Wall},
+				{ TileType.Wall, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Wall, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Wall},
+				{ TileType.Wall, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Wall, TileType.Ground, TileType.Ground, TileType.Button, TileType.Ground, TileType.Wall},
+				{ TileType.Wall, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Wall, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Ground, TileType.Wall},
+				{ TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall}, 
 			};
 
-			StartPosition = new Vector2Int(1, 1);
+			StartPosition = new Vector2Int(7, 2);
 
 			CrateStartPosition = new[]
-				{ new Vector2Int(3, 3), new Vector2Int(4, 3), new Vector2Int(4, 4), new Vector2Int(5, 5) };
+				{ new Vector2Int(5, 7) };
 		}
 	}
 }
