6b42a32ae3225505ca4d2a81ed91b4a485be277c
Lior <diler.lior@gmail.com>
2022-03-14 02:18:22 +0100

    added level 5

---
+++ Assets/Sokoban/Scripts/LevelPresets/Level5Preset.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Sokoban
+{
+    public class Level5Preset : ALevelPreset
+    {
+        public Level5Preset()
+        {
+            Grid = new TileType[,]{
+                { TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall},
+                { TileType.Wall, TileType.Ground, TileType.Ground, TileType.Wall, TileType.Wall},
+                { TileType.Wall, TileType.Ground, TileType.Ground, TileType.Ground , TileType.Wall},
+                { TileType.Wall, TileType.Button, TileType.Ground, TileType.Ground, TileType.Wall},
+                { TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall, TileType.Wall},
+            };
+
+            StartPosition = new Vector2Int(1, 1);
+
+            CrateStartPosition = new[]
+                { new Vector2Int(2, 2) };
+        }
+    }
+}
